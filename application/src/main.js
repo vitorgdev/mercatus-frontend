9421cd4aa2e0dd5f896a254dbcdb6ee461c39724
vitorgdev <vitorg.dev@gmail.com>
2019-07-10 09:51:28 -0300

    Commit Inicial

--- application/src/main.js
+++ application/src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import BootstrapVue from 'bootstrap-vue'
+import './assets/scss/medical.scss'
+
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
--- application/src/modules/auth/index.vue
+++ application/src/modules/auth/index.vue
@@ -1,13 +1,64 @@
 <template>
-  <ChatList :messages="messages" />
+  <div class="auth-background">
+    <b-container>
+      <b-row>
+        <b-col cols="6">
+          <b-row>
+            <b-col
+              cols="2"
+              align-self="center"
+            >
+              <img
+                :src="logo"
+                alt="logo"
+                width="80%"
+              >
+            </b-col>
+
+            <b-col
+              class="identity"
+              align-self="center"
+            >
+              <span class="text-white display-5">kokkua</span>
+            </b-col>
+          </b-row>
+          <b-row style="margin-top:20%">
+            <b-col>
+              <span class="title red-line text-grey">kokkua apresenta</span>
+            </b-col>
+          </b-row>
+          <b-row class="mt-3">
+            <b-col cols="8">
+              <span class="description text-white">toda sua empresa em um sistema.</span>
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col
+              cols="8"
+              class="text-right"
+            >
+              <span class="title text-grey">saiba mais <img :src="arrow" alt=""></span>
+            </b-col>
+          </b-row>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <script>
 // import { mapGetters } from 'vuex'
 // import store from './_store'
-
+import Logo from "../../assets/images/logo.svg";
+import Arrow from "../../assets/images/arrow.svg";
 export default {
-  name: 'ChatModule'
+  name: 'AuthScreen',
+  data () {
+    return {
+      logo: Logo,
+      arrow: Arrow
+    }
+  }
   // components: {
   //   ChatList
   // },
@@ -28,3 +79,40 @@ export default {
   // }
 }
 </script>
+<style lang="scss" scoped>
+.auth-background {
+  background-image: url("../../assets/images/background-authentication.svg");
+  background-position: center; /* Center the image */
+  background-repeat: no-repeat; /* Do not repeat the image */
+  background-size: cover;
+  height: 100vh;
+}
+.container {
+  padding-top: 3%;
+  .identity {
+    span {
+      font-size: 42px;
+      font-weight: bold;
+    }
+  }
+  .title {
+    font-size: 28px;
+    color: #dfdfe2;
+    &.red-line {
+      &:before {
+        content: "";
+        position: absolute;
+        width: 20%;
+        /* height: 0px; */
+        border: 1px solid red;
+        top: 40px;
+      }
+    }
+  }
+  .description {
+    font-size: 60px;
+    font-family: "Inconsolata", monospace;
+    line-height: 60px;
+  }
+}
+</style>
