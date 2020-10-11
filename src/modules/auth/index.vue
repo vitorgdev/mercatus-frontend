9c73454b48318057ed05fb2ad9e9e5650754ecd5
Vitor Meira Gomes <vitorg.dev@gmail.com>
2020-10-11 04:33:12 -0300

    Validação de form feita

--- src/modules/auth/index.vue
+++ src/modules/auth/index.vue
@@ -20,20 +20,43 @@
         <v-card-text>
           <v-row>
             <v-col>
-              <v-text-field label="Usuário" filled></v-text-field>
-              <v-text-field
-                :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-                @click:append="showPassword = !showPassword"
-                :type="showPassword ? 'text' : 'password'"
-                label="Senha"
-                filled
-              ></v-text-field>
+              <validation-observer ref="observer">
+                <v-form>
+                  <validation-provider
+                    v-slot="{ errors }"
+                    name="Usuário"
+                    rules="required"
+                  >
+                    <v-text-field
+                      label="Usuário"
+                      :error-messages="errors"
+                      v-model="form.username"
+                      filled
+                    ></v-text-field>
+                  </validation-provider>
+                  <validation-provider
+                    v-slot="{ errors }"
+                    name="Senha"
+                    rules="required"
+                  >
+                    <v-text-field
+                      v-model="form.password"
+                      :error-messages="errors"
+                      :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                      @click:append="showPassword = !showPassword"
+                      :type="showPassword ? 'text' : 'password'"
+                      label="Senha"
+                      filled
+                    ></v-text-field>
+                  </validation-provider>
+                </v-form>
+              </validation-observer>
             </v-col>
           </v-row>
         </v-card-text>
 
         <v-card-actions>
-          <v-btn x-large expansed color="primary" tile block>
+          <v-btn @click="submit" x-large expansed color="primary" tile block>
             Entrar
           </v-btn>
         </v-card-actions>
@@ -43,12 +66,41 @@
 </template>
 
 <script>
+import { required } from "vee-validate/dist/rules";
+import {
+  extend,
+  ValidationObserver,
+  ValidationProvider,
+  setInteractionMode
+} from "vee-validate";
+
+setInteractionMode("eager");
+
+extend("required", {
+  ...required,
+  message: "{_field_} não pode ser vazio(a)"
+});
+
 export default {
   name: "AuthModule",
+  components: {
+    ValidationProvider,
+    ValidationObserver
+  },
   data() {
     return {
+      errors: null,
+      form: {
+        username: "",
+        password: ""
+      },
       showPassword: false
     };
+  },
+  methods: {
+    submit() {
+      this.$refs.observer.validate();
+    }
   }
 };
 </script>
